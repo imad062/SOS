7518e38b9c55c8a303ce47e5671862422da43581
imad062 <nooruddinimad@gmail.com>
2017-12-02 18:27:39 +0600

    designed layout

--- app/src/main/java/com/example/imad/sos/MainActivity.java
+++ app/src/main/java/com/example/imad/sos/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.imad.sos;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
+        this.getSupportActionBar().hide();
     }
 }
